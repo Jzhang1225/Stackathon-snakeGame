3fd6a7726fc0d7385bd83f1d96f71fd45c86d18e
Jianing Zhang <jzhang718@gmail.com>
2022-05-19 23:20:57 -0400

    added path to assets folder

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require("path");
 
 app.use("/dist", express.static(path.join(__dirname, "dist")));
+app.use("/assets", express.static(path.join(__dirname, "assets")));
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
 
 const port = process.env.PORT || 3000;
